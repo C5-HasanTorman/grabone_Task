a64bfb3154b21d17bdb770eb4742a5b327c1f2a5
HasanTorman <torman.hassan@gmail.com>
2022-09-02 03:43:30 +0300

    create Activities component

--- app-name/src/App.js
+++ app-name/src/App.js
@@ -6,16 +6,18 @@ import SendBox from "./Component/MainPage/SendBox";
 import Deals from "./Component/MainPage/Deals";
 import Collections from "./Component/MainPage/Collections";
 import Escapes from "./Component/MainPage/Escapes";
+import Activities from "./Component/MainPage/Activities";
 function App() {
   return (
     <div class="container-fluid">
       <Navbar />
       <ImgHead />
       <MainTop />
-      <SendBox/>
-      <Deals/>
-      <Collections/>
-      <Escapes/>
+      <SendBox />
+      <Deals />
+      <Collections />
+      <Escapes />
+      <Activities />
     </div>
   );
 }
